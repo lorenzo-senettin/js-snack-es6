884f34fed102115d3fb64c1e92211bcc6f8c282c
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-27 13:41:49 +0100

    add array with teams

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,20 +1,52 @@
-// value limit
-const limit = 6;
-
-// user number
-let userNumber;
-userNumber = Number(prompt("Inserisci un numero da 1 a 6:"));
-
-// pc number
-const pcNumber = Math.ceil(Math.random() * limit)
-
-console.log(userNumber, pcNumber);
-
-
-if (userNumber === pcNumber) {
-  console.log(`C'è stato un pareggio`)
-} else if (userNumber > pcNumber) {
-  console.log(`GG hai vinto`);
-} else {
-  console.log(`Il pc ha vinto. XD`)
-}
+const teams = [
+  {
+    nome: "Juventus",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Inter",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Milan",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Roma",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Napoli",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Lazio",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Atalanta",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Fiorentina",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Sampdoria",
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: "Torino",
+    puntiFatti: 0,
+    falliSubiti: 0
+  }
+];
